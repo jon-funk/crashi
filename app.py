ae18b62f5ff8ee3ada31a0db08d8221806db8642
Jonathan Funk <jonathan.funk@runwhen.com>
2023-10-24 22:36:31 -0700

    Log exceptions in full to logs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pycron, os, logging
 from datetime import datetime
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+
 
 CRASH_SCHEDULE = os.getenv("CRASH_SCHEDULE", "* * * * *")
 SIM_ENV_MISSING = os.getenv("SIM_ENV_MISSING", "NO")
@@ -17,6 +19,14 @@ app = FastAPI(
     description="A simple API for returning internal server errors on a schedule for reliability testing.",
     version="v0.1",
 )
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.error(f"Exception: {exc.detail}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
 
 @app.get("/api/health")
 async def health():
