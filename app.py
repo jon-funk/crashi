ae535cbcd62fd70894ba620c31eadf1469b7649c
Jonathan Funk <jonathan.funk@runwhen.com>
2023-11-21 11:08:41 -0800

    Add customizable exception string

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pycron, os, logging
+import pycron, os, logging, traceback
 from datetime import datetime
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
@@ -7,6 +7,7 @@ from fastapi.responses import JSONResponse
 CRASH_SCHEDULE = os.getenv("CRASH_SCHEDULE", "* * * * *")
 SIM_ENV_MISSING = os.getenv("SIM_ENV_MISSING", "NO")
 SIM_HIDDEN_ENV_MISSING = os.getenv("SIM_HIDDEN_ENV_MISSING", "NO")
+SIM_CATCHALL_RSP = os.getenv("SIM_CATCHALL_RSP", "This internal server error at {datetime.now()} is expected as SIM_HIDDEN_ENV_MISSING is set to {SIM_HIDDEN_ENV_MISSING} indicating that a missing feature flag environment variable should be set. {stacktrace}")
 
 logger = logging.getLogger(__name__)
 logger.info(f"Crash schedule set to: {CRASH_SCHEDULE}\nSimulating missing env crash: {SIM_ENV_MISSING}\nSimulating hidden missing env: {SIM_HIDDEN_ENV_MISSING}")
@@ -41,18 +42,26 @@ async def health():
             "detail": f"{datetime.now()} is not within the crash schedule '{CRASH_SCHEDULE}'"
         }
 
+
 @app.get("/api/simenv")
 async def sim_env():
     if SIM_HIDDEN_ENV_MISSING != "NO":
-        raise HTTPException(
-            status_code=500,
-            detail=f"This internal server error at {datetime.now()} is expected as SIM_HIDDEN_ENV_MISSING is set to {SIM_HIDDEN_ENV_MISSING} indicating that a missing feature flag environment variable should be set."
-        )
+        try:
+            raise Exception("Endpoint has encountered a fatal error while processing the request.")
+        except Exception as e:
+            stack_trace = traceback.format_exc()
+            # Dynamic evaluation of SIM_CATCHALL_RSP with stack trace
+            detail_message = SIM_CATCHALL_RSP.format(datetime=datetime.now(), SIM_HIDDEN_ENV_MISSING=SIM_HIDDEN_ENV_MISSING, stack_trace=stack_trace)
+            raise HTTPException(
+                status_code=500,
+                detail=detail_message
+            )
     else:
         return {
             "message": "OK",
-            "detail": f"Not simulating a missing feature flag - returning OK"
+            "detail": "Not simulating a missing feature flag - returning OK"
         }
+
 
 @app.api_route("/{path_name:path}", methods=["GET"])
 async def catch_all(request: Request, path_name: str):
